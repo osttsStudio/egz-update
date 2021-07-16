4b86a9ca7d9e8ab0dc9638baf2d3e93896265b80
狐日泽 <Sun_Set1102@outlook.com>
2021-07-16 18:01:00 +0800

    v2.2.5

--- update.bat
+++ update.bat
@@ -1,4 +1,5 @@
-move .minecraft\start.exe .\
 move .minecraft\modlist.xlsx .\
 move .minecraft\config.ini .\
+move .minecraft\start.exe .\
 del /f .minecraft\mods\OpenComputers-MC1.12.2-1.7.5.192.jar
+pause
